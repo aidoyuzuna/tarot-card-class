742456ba0d3e48a5c6798c144cd42f03c818d6c4
saro <netsv1643+gi@gmail.com>
2023-09-17 22:56:39 +0900

    Enumを活用まで進める

--- tarot-card-class.py
+++ tarot-card-class.py
@@ -1,31 +1,59 @@
+from enum import Enum, auto
+
+
+class Suit(Enum):
+    WAND = "ワンド"
+    PENTACLE = "ペンタクル"
+    SWORD = "ソード"
+    CUP = "カップ"
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return f"Suit.{self.name}"
+
+
+class Number(Enum):
+    ACE = (1, "A")
+    TWO = (2, "2")
+    THREE = (3, "3")
+    FOUR = (4, "4")
+    FIVE = (5, "5")
+    SIX = (6, "6")
+    SEVEN = (7, "7")
+    EIGHT = (8, "8")
+    NINE = (9, "9")
+    TEN = (10, "10")
+    KNIHT = (11, "J")
+    PAGE = (12, "P")
+    QUEEN = (13, "Q")
+    KING = (14, "K")
+
+    def __init__(self, val, string):
+        self.val = val
+        self.string = string
+
+    def __str__(self):
+        return self.string
+
+    def __repr__(self):
+        return f"Number.(self.name)"
+
+
 class Card:
     def __init__(self, suit, number):
+        if not (isinstance(suit, Suit) and isinstance(number, Number)):
+            raise ValueError
         self.suit = suit
         self.number = number
 
     def __str__(self):
-        suits_str = ["ワンド", "ペンタクル", "ソード", "カップ"][self.suit]
-        number_str = [
-            "エース",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "ナイト",
-            "ペイジ",
-            "クイーン",
-            "キング",
-        ][self.number]
-        return suits_str + number_str
+        return str(self.suit) + str(self.number)
 
     def __repr__(self):
-        return f"Card({self.suit},{self.number})"
+        return self.__str__()
 
 
-minor_arcana = [Card(suit, number) for suit in range(4) for number in range(1, 14)]
+minor_arcana = [Card(suit, number) for suit in Suit for number in Number]
 print(minor_arcana)
