79f3f01158d7e1339f1cc13916dbc37bcf19647b
saro <netsv1643+gi@gmail.com>
2023-09-17 22:42:02 +0900

    クラス化してみた

--- tarot-card-class.py
+++ tarot-card-class.py
@@ -1,21 +1,31 @@
-minor_arcana = []
-suits = ["ワンド", "ペンタクル", "ソード", "カップ"]
-numbers = [
-    "エース",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "ナイト",
-    "ペイジ",
-    "クイーン",
-    "キング",
-]
+class Card:
+    def __init__(self, suit, number):
+        self.suit = suit
+        self.number = number
 
-minor_arcana = [(suits, numbers) for suits in range(4) for numbers in range(1, 14)]
+    def __str__(self):
+        suits_str = ["ワンド", "ペンタクル", "ソード", "カップ"][self.suit]
+        number_str = [
+            "エース",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "ナイト",
+            "ペイジ",
+            "クイーン",
+            "キング",
+        ][self.number]
+        return suits_str + number_str
+
+    def __repr__(self):
+        return f"Card({self.suit},{self.number})"
+
+
+minor_arcana = [Card(suit, number) for suit in range(4) for number in range(1, 14)]
 print(minor_arcana)
